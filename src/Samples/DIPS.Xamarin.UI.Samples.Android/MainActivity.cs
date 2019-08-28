016b3cff58bec69a7cf6ceeb42fb25b1243705b1
Håvard Moås <hmo@dips.no>
2019-08-28 16:06:02 +0200

    Added initial project for DIPS.Xamarin.UI (#4)
    Added Xamarin.Forms Sample project

---
+++ src/Samples/DIPS.Xamarin.UI.Samples.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Essentials = Xamarin.Essentials;
+using Content = Android.Content;
+
+namespace DIPS.Xamarin.UI.Samples.Droid
+{
+    [Activity(Label = "DIPS.Xamarin.UI.Samples", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Essentials.Platform.Init(this, savedInstanceState); //Xamarin essentials
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Content.PM.Permission[] grantResults)
+        {
+            Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults); //Xamarin essentials
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
