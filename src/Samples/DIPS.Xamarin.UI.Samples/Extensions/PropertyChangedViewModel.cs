68f81c0260917a1c2db58c81d3e3ded7d80ea795
Håvard Moås <hmo@dips.no>
2019-09-18 08:32:01 +0200

    Added PropertyChanged extensions with tests
    Missing a couple of tests

---
+++ src/Samples/DIPS.Xamarin.UI.Samples/Extensions/PropertyChangedViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using DIPS.Xamarin.UI.Extensions;
+
+namespace DIPS.Xamarin.UI.Samples.Extensions
+{
+    /// <summary>
+    ///     This sample shows how we can use the <see cref="PropertyChangedExtensions" /> in order to simplify how to notify
+    ///     property changed
+    /// </summary>
+    internal class PropertyChangedViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string m_myFirstProperty;
+        private string m_mySecondProperty;
+        private string m_myThirdProperty;
+
+        /// <summary>
+        /// Notifies with a single Set method
+        /// </summary>
+        public string MyFirstProperty
+        {
+            get => m_myFirstProperty;
+            set => this.Set(ref m_myFirstProperty, value, PropertyChanged);
+        }
+
+        /// <summary>
+        /// Notifies with a OnPropertyChanged method
+        /// </summary>
+        public string MySecondProperty
+        {
+            get => m_mySecondProperty;
+            set
+            {
+                m_mySecondProperty = value;
+                this.OnPropertyChanged(PropertyChanged);
+            }
+        }
+
+        /// <summary>
+        /// Notifies that multiple properties has changed
+        /// </summary>
+        public string MyThirdProperty
+        {
+            get => m_myThirdProperty;
+            set
+            {
+                m_myThirdProperty = value;
+                this.OnMultiplePropertiesChanged(PropertyChanged, 
+                    nameof(MyFirstProperty), 
+                    nameof(MySecondProperty), 
+                    nameof(MyThirdProperty));
+            }
+        }
+    }
+}
---
+++ src/Tests/DIPS.Xamarin.UI.Tests/Extensions/PropertyChangedExtensionsTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using DIPS.Xamarin.UI.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace DIPS.Xamarin.UI.Tests.Extensions
+{
+    public class PropertyChangedExtensionsTests : INotifyPropertyChanged
+    {
+        private const string InitialValue = "Initial Value";
+        private const string NewValue = "New Value";
+        private string m_myFirstProperty;
+
+        public PropertyChangedExtensionsTests()
+        {
+            MyFirstProperty = InitialValue;
+        }
+
+        public string MyFirstProperty
+        {
+            get => m_myFirstProperty;
+            set => this.Set(ref m_myFirstProperty, value, PropertyChanged);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [Fact]
+        public void Set_NewValueIsNotEqualToOldValue_NotifyingPropertyChanged()
+        {
+            var result = false;
+            void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                result = true;
+            }
+
+            PropertyChanged += OnPropertyChanged;
+
+            MyFirstProperty = NewValue;
+
+            PropertyChanged -= OnPropertyChanged;
+
+            result.Should().BeTrue(because:"Value is not the same as initial value of the property");
+        }
+
+        [Fact]
+        public void Set_NewValueIsEqualToOldValue_NotNotifyingPropertyChanged()
+        {
+            var result = false;
+            void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                result = true;
+            }
+
+            PropertyChanged += OnPropertyChanged;
+
+            MyFirstProperty = InitialValue;
+
+            PropertyChanged -= OnPropertyChanged;
+
+            result.Should().BeFalse(because:"Value was the same as initial value of the property");
+        }
+
+        [Fact]
+        public void Set_NewValue_BackingStoreHasSameValue()
+        {
+            MyFirstProperty = NewValue;
+
+            m_myFirstProperty.Should().Be(NewValue, because: "The set method will always set the value to a new value, or keep te same value");
+        }
+
+        [Fact]
+        public void OnPropertyChanged_NotifiesPropertyChanged()
+        {
+            var result = false;
+            void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                result = e.PropertyName.Equals(nameof(MyFirstProperty));
+            }
+
+            PropertyChanged += OnPropertyChanged;
+
+            this.OnPropertyChanged(PropertyChanged, nameof(MyFirstProperty));
+
+            PropertyChanged -= OnPropertyChanged;
+
+            result.Should().BeTrue(because: "PropertyChanged should always be raised with the correct property");
+        }
+    }
+}
