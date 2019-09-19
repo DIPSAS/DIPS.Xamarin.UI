5954bc5df713ae428246f279bcbb88c588e7c563
Håvard Moås <hmo@dips.no>
2019-09-19 14:44:57 +0200

    Added the option to use Custom Namespace, Fixes #1 (#14)

---
+++ src/DIPS.Xamarin.UI/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIPS.Xamarin.UI
+{
+    /// <summary>
+    /// A static class to initialize DIPS.Xamarin.UI Library
+    /// </summary>
+    public static class Library
+    {
+        /// <summary>
+        /// Initializes the DIPS.Xamarin.UI Library, enables custom namespace http://dips.xamarin.ui.com to be used in XAML.
+        /// <remarks>This should be called once per application, typically in the `App.xaml.cs` constructor. </remarks>
+        /// </summary>
+        public static void Initialize(){}
+    }
+}
