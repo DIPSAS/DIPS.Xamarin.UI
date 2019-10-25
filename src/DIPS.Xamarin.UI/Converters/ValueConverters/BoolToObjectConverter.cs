4991ddf2d08c6ea8fa744dc1b41b1407c0b0e404
Håvard Moås <hmo@dips.no>
2019-10-25 08:47:33 +0200

    Added BoolToObjectConverter

---
+++ src/DIPS.Xamarin.UI/Converters/ValueConverters/BoolToObjectConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.UI.Converters.ValueConverters
+{
+    /// <summary>
+    /// Converters an boolean input value to it's respective <see cref="TrueObject"/> or <see cref="FalseObject"/> depending on the <see cref="Inverted"/> value
+    /// </summary>
+    public class BoolToObjectConverter : IValueConverter, IMarkupExtension
+    {
+        /// <summary>
+        /// The value that will return if the boolean input is true
+        /// <remarks>Will be the return value if <see cref="Inverted"/> is set to true</remarks>
+        /// </summary>
+        public object? TrueObject { get; set; }
+        /// <summary>
+        /// The value that will return if the boolean input is false
+        /// <remarks>Will be the return value if <see cref="Inverted"/> is set to false</remarks>
+        /// </summary>
+        public object? FalseObject { get; set; }
+        /// <summary>
+        /// A boolean value to turn on or inverting of the output values
+        /// </summary>
+        public bool Inverted { get; set; }
+
+        /// <inheritdoc />
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool inputValue)) throw new ArgumentException($"Input value has to be of type {nameof(Boolean)}");
+            if (TrueObject?.GetType() != FalseObject?.GetType())
+                throw new ArgumentException($"{nameof(TrueObject)} has to be the same type as {FalseObject}");
+
+            return Inverted ? (inputValue) ? FalseObject : TrueObject : (inputValue) ? TrueObject : FalseObject;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ src/Samples/DIPS.Xamarin.UI.Samples/Converters/ConvertersViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using DIPS.Xamarin.UI.Samples.Controls.DatePicker;
+using DIPS.Xamarin.UI.Samples.Converters.ValueConverters;
+using Xamarin.Forms;
+
+namespace DIPS.Xamarin.UI.Samples.Converters
+{
+    public class ConvertersViewModel
+    {
+        private readonly INavigation m_navigation;
+
+        public ConvertersViewModel(INavigation navigation)
+        {
+            m_navigation = navigation;
+            NavigateToCommand = new Command<string>(NavigateTo);
+        }
+
+        public ICommand NavigateToCommand { get; }
+
+        private void NavigateTo(string parameter)
+        {
+            switch (parameter)
+            {
+                case "BoolToObject":
+                    m_navigation.PushAsync(new BoolToObjectConverterPage());
+                    break;
+                case "InvertedBool":
+                    m_navigation.PushAsync(new InvertedBoolConverterPage());
+                    break;
+                case "IsEmptyConverter":
+                    m_navigation.PushAsync(new IsEmptyConverterPage());
+                    break;
+            }
+        }
+    }
+}
