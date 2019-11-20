7a2503f41b5b8d64496d0b79a74733f124e56476
Erik Kvanli <0erikkvanli0@gmail.com>
2019-11-20 19:19:30 +0530

    Started on unit tests

---
+++ src/Tests/DIPS.Xamarin.UI.Tests/Extensions/PositionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DIPS.Xamarin.UI.Extensions;
+using FluentAssertions;
+using Xamarin.Forms;
+using Xunit;
+
+namespace DIPS.Xamarin.UI.Tests.Extensions
+{
+    public class PositionExtensions
+    {
+        [Fact]
+        public void NoParent_ChildsX()
+        {
+            var child = new Frame();
+            var relativeLayout = new RelativeLayout();
+            relativeLayout.Children.Add(child, Constraint.RelativeToParent(r => 42));
+
+            var x = child.GetX(relativeLayout);
+
+            x.Should().Be(0);
+        }
+    }
+}
