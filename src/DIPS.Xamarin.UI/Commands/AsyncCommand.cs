0c0c5218e1221bc25bd05387bc44dcbede422491
Håvard Moås <hmo@dips.no>
2019-11-21 15:10:34 +0100

    Added xml doc, fixed release configuration to include doc and warnings as errors

--- src/DIPS.Xamarin.UI/Commands/AsyncCommand.cs
+++ src/DIPS.Xamarin.UI/Commands/AsyncCommand.cs
@@ -3,31 +3,46 @@ using System.Threading.Tasks;
 
 namespace DIPS.Xamarin.UI.Commands
 {
+    /// <inheritdoc />
     public class AsyncCommand : IAsyncCommand
     {
         private readonly Func<Task> m_execute;
         private readonly Func<bool> m_canExecute;
         private readonly Action<Exception> m_onException;
 
-        public AsyncCommand(Func<Task> execute, Action<Exception>? onExceptipon = null)
+        /// <summary>
+        /// Constructs an <see cref="AsyncCommand"/>
+        /// </summary>
+        /// <param name="execute">The task to execute</param>
+        /// <param name="onException">The action to run if the async command throws outside of the task</param>
+        public AsyncCommand(Func<Task> execute, Action<Exception>? onException = null)
         {
             m_execute = execute;
             m_canExecute = () => true;
-            m_onException = onExceptipon ?? (e => { });
+            m_onException = onException ?? (e => { });
         }
 
-        public AsyncCommand(Func<Task> execute, Func<bool> canExecute, Action<Exception>? onExceptipon = null)
+        /// <summary>
+        /// Constructs an <see cref="AsyncCommand"/>
+        /// </summary>
+        /// <param name="execute">The task to execute</param>
+        /// <param name="canExecute">An func to determine if the command can execute</param>
+        /// <param name="onException">The action to run if the async command throws outside of the task</param>
+        public AsyncCommand(Func<Task> execute, Func<bool> canExecute, Action<Exception>? onException = null)
         {
             m_execute = execute;
             m_canExecute = canExecute;
-            m_onException = onExceptipon ?? (e => { });
+            m_onException = onException ?? (e => { });
         }
 
+        /// <inheritdoc />
         public bool CanExecute(object parameter) => m_canExecute();
 
+        /// <inheritdoc />
         public Task ExecuteAsync() => m_execute();
 
 #pragma warning disable RECS0165
+        /// <inheritdoc />
         public async void Execute(object parameter)
         {
             try
@@ -46,33 +61,48 @@ namespace DIPS.Xamarin.UI.Commands
         }
 #pragma warning restore RECS0165
 
+        /// <inheritdoc />
         public void RaiseCanExecuteChanged()
         {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <inheritdoc />
         public event EventHandler? CanExecuteChanged;
     }
 
+    /// <inheritdoc />
     public class AsyncCommand<T> : IAsyncCommand<T>
     {
         private readonly Func<T, Task> m_execute;
         private readonly Func<T, bool> m_canExecute;
         private readonly Action<Exception> m_onException;
-        public AsyncCommand(Func<T, Task> execute, Action<Exception>? onExceptipon = null)
+        /// <summary>
+        /// Constructs an <see cref="AsyncCommand{T}"/>
+        /// </summary>
+        /// <param name="execute">The task to execute</param>
+        /// <param name="onException">The action to run if the async command throws outside of the task</param>
+        public AsyncCommand(Func<T, Task> execute, Action<Exception>? onException = null)
         {
             m_execute = execute;
             m_canExecute = value => true;
-            m_onException = onExceptipon ?? (e => { });
+            m_onException = onException ?? (e => { });
         }
 
-        public AsyncCommand(Func<T, Task> execute, Func<T, bool> canExecute, Action<Exception>? onExceptipon = null)
+        /// <summary>
+        /// Constructs an <see cref="AsyncCommand{T}"/>
+        /// </summary>
+        /// <param name="execute">The task to execute</param>
+        /// <param name="canExecute">An func to determine if the command can execute</param>
+        /// <param name="onException">The action to run if the async command throws outside of the task</param>
+        public AsyncCommand(Func<T, Task> execute, Func<T, bool> canExecute, Action<Exception>? onException = null)
         {
             m_execute = execute;
             m_canExecute = canExecute;
-            m_onException = onExceptipon ?? (e => { });
+            m_onException = onException ?? (e => { });
         }
 
+        /// <inheritdoc />
         public bool CanExecute(object parameter)
         {
             if(!(parameter is T value))
@@ -83,9 +113,11 @@ namespace DIPS.Xamarin.UI.Commands
             return m_canExecute(value);
         }
 
+        /// <inheritdoc />
         public Task ExecuteAsync(T value) => m_execute(value);
 
 #pragma warning disable RECS0165
+        /// <inheritdoc />
         public async void Execute(object parameter)
         {
             try
@@ -104,11 +136,13 @@ namespace DIPS.Xamarin.UI.Commands
         }
 #pragma warning restore RECS0165
 
+        /// <inheritdoc />
         public void RaiseCanExecuteChanged()
         {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <inheritdoc />
         public event EventHandler? CanExecuteChanged;
     }
 }
