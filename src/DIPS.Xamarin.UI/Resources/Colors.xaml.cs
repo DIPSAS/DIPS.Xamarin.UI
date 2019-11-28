a0370f7ddc915ed9dc06b6a9a6698a0a5b6de2b7
Håvard Moås <hmo@dips.no>
2019-11-28 11:16:55 +0100

    Added Colors

---
+++ src/DIPS.Xamarin.UI/Resources/Colors.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.UI.Resources
+{
+    /// <summary>
+    /// ResourceDictionary of the common colors
+    /// </summary>
+    /// <remarks>
+    /// See wiki: https://github.com/DIPSAS/DIPS.Xamarin.UI/wiki/Colors
+    /// </remarks>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Colors : ResourceDictionary
+    {
+        /// <summary>
+        /// Constructs the common colors.
+        /// </summary>
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
