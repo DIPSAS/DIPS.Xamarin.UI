fc7ed6428614edae8ff8b9d643a8e83e591b6cc5
Håvard Moås <hmo@dips.no>
2019-11-28 11:44:14 +0100

    Added more colors and added sample page

---
+++ src/Samples/DIPS.Xamarin.UI.Samples/Resources/Colors/ColorsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.UI.Samples.Resources.Colors
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ColorsPage : ContentPage
+    {
+        public ColorsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Samples/DIPS.Xamarin.UI.Samples/Resources/ResourcesPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.UI.Samples.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResourcesPage : ContentPage
+    {
+        public ResourcesPage()
+        {
+            InitializeComponent();
+            BindingContext = new ResourcesPageViewModel(Application.Current.MainPage.Navigation);
+        }
+    }
+}
