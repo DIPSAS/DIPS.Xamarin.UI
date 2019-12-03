8338975f12af1772bf0f8ccbcc57126d232b5672
Håvard Moås <hmo@dips.no>
2019-12-03 09:05:17 +0100

    Fixed height consumption bug

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github64/Github64.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github64
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Github64 : ContentPage
+    {
+        public Github64()
+        {
+            InitializeComponent();
+        }
+    }
+}
