19ca9fa1fcc2c01d65a959d31eb4967a4e8d37a0
Håvard Moås <hmo@dips.no>
2019-12-20 11:19:17 +0100

    Added DateConverter, TimeConverter and DateAndTimeConverter (#77)
    * Added Date / DateTime Converters with tests
    
    * Added date converters
    
    * Added Text format for DateConverter
    
    * Improved date picker for dateconverterpage
    
    * Added tests for DateConverter.Text
    
    * Added Short format, which is the same as default
    
    * Added TimeConverter and refactored + added tests
    
    * Added comments to converters
    
    * Formatted code
    
    * Formatted code and added more xml doc
    
    * Formatted switches
    
    * Fixed TimeConverter in norwegian and added date and time pickers for DateTimeConverter sample
    
    * Fixed TimeConverter
    
    * Added the usage of `Clock` in our extensions in order for us to make sure that we can control the time in our unit tests
    
    Renamed GetDaySuffix to GetEnglishDaySuffix
    
    * Disable fake nullable warning because csharp8 doesnt understand that s_clock is not null
    
    * Removed copy-pasted namespace

---
+++ src/DIPS.Xamarin.UI/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using DIPS.Xamarin.UI.InternalUtils;
+
+namespace DIPS.Xamarin.UI.Extensions
+{
+    /// <summary>
+    /// Extensions class for DateTime
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+
+        /// <summary>
+        /// Checks if the date occurred to day
+        /// </summary>
+        /// <param name="dateTime">The datetime to check</param>
+        /// <returns>Boolean value</returns>
+        public static bool IsToday(this DateTime dateTime) => dateTime.Date == Clock.Today.Date;
+
+        /// <summary>
+        /// Checks if the date occurred yesterday
+        /// </summary>
+        /// <param name="dateTime">The datetime to check</param>
+        /// <returns>Boolean value</returns>
+        public static bool IsYesterday(this DateTime dateTime)
+        {
+            return dateTime.Date == Clock.Now.AddDays(-1).Date;
+        }
+
+        /// <summary>
+        /// Checks if the date occurs tomorrow
+        /// </summary>
+        /// <param name="dateTime">The datetime to check</param>
+        /// <returns>Boolean value</returns>
+        public static bool IsTomorrow(this DateTime dateTime)
+        {
+            return dateTime.Date == Clock.Now.AddDays(1).Date;
+        }
+
+        /// <summary>
+        /// Gets the correct english day suffix for a date
+        /// </summary>
+        /// <param name="dateTime">The datetime to get the suffix from</param>
+        /// <returns>a string with the correct suffix</returns>
+        public static string GetEnglishDaySuffix(this DateTime dateTime)
+        {
+            switch (dateTime.Day)
+            {
+                case 1:
+                case 21:
+                case 31:
+                    return "st";
+                case 2:
+                case 22:
+                    return "nd";
+                case 3:
+                case 23:
+                    return "rd";
+                default:
+                    return "th";
+            }
+        }
+    }
+}
---
+++ src/Samples/DIPS.Xamarin.UI.Samples/Converters/ValueConverters/DateConverterPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DIPS.Xamarin.UI.Extensions;
+using DIPS.Xamarin.UI.InternalShared;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.UI.Samples.Converters.ValueConverters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateConverterPage : ContentPage
+    {
+        public DateConverterPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class DateConverterPageViewModel : INotifyPropertyChanged
+    {
+        public DateConverterPageViewModel()
+        {
+
+            OpenLocaleMobileSettingsCommand = new Command(() => MobileSettings.Instance.OpenLocale());
+            Date = DateTime.Now;
+        }
+
+        private DateTime m_date;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime Date
+        {
+            get => m_date;
+            set => PropertyChanged.RaiseAfter(ref m_date, value);
+        }
+
+        public ICommand OpenLocaleMobileSettingsCommand { get; }
+
+        public string Locale => System.Threading.Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
+    }
+}
