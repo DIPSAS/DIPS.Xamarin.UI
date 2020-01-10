4936f2a88d1d6c8e5a2576d9f86d279ef78b24ab
Håvard Moås <hmo@dips.no>
2020-01-10 10:56:35 +0100

    Changed Factor for multiplicationconverter to double to prevent potential culture parsings

--- src/DIPS.Xamarin.UI/Converters/ValueConverters/MultiplicationConverter.cs
+++ src/DIPS.Xamarin.UI/Converters/ValueConverters/MultiplicationConverter.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Numerics;
-using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,20 +16,17 @@ namespace DIPS.Xamarin.UI.Converters.ValueConverters
         /// <summary>
         /// The factor to multiply with
         /// </summary>
-        /// <remarks>This has to be number</remarks>
-        public object? Factor { get; set; }
+        public double? Factor { get; set; }
 
         /// <inheritdoc />
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null) throw new ArgumentException("Value is null");
-            if (Factor == null) throw new ArgumentException("Factor is null, it has to be a number");
+            if (Factor == null) throw new ArgumentException("Factor is null, it has to be a double");
             if (!double.TryParse(value.ToString(), out var number))
-                throw new InvalidOperationException("Value is not a number.");
-            if (!double.TryParse(Factor.ToString(), out var factor))
-                throw new InvalidOperationException("Value is not a number.");
+                throw new ArgumentException("Value is not a number");
 
-            return number * factor;
+            return number * Factor;
         }
 
         /// <inheritdoc />
