59ca85c373b222139e584a39d6c8f60bb817e318
Håvard Moås <hmo@dips.no>
2020-01-21 09:46:33 +0100

    Added xml comments for sheet behavior

--- src/DIPS.Xamarin.UI/Controls/Sheet/SheetBehavior.cs
+++ src/DIPS.Xamarin.UI/Controls/Sheet/SheetBehavior.cs
@@ -16,23 +16,32 @@ namespace DIPS.Xamarin.UI.Controls.Sheet
 
         private SheetView? m_sheetView;
 
-
+        /// <summary>
+        /// <see cref="VerticalContentAlignment"/>
+        /// </summary>
         public static readonly BindableProperty VerticalContentAlignmentProperty = BindableProperty.Create(nameof(VerticalContentAlignment), typeof(ContentAlignment), typeof(SheetBehavior), ContentAlignment.Fit);
 
         /// <summary>
-        /// Settings this property changes how much of the space the content should take of the entire sheet. 
+        /// Determines how the content of the sheet should align.
+        /// This is a bindable property.
         /// </summary>
         public ContentAlignment VerticalContentAlignment
         {
             get => (ContentAlignment)GetValue(VerticalContentAlignmentProperty);
             set => SetValue(VerticalContentAlignmentProperty, value);
         }
+        /// <summary>
+        /// <see cref="Alignment"/>
+        /// </summary>
         public static readonly BindableProperty AlignmentProperty = BindableProperty.Create(
             nameof(Alignment),
             typeof(AlignmentOptions),
             typeof(SheetView),
             AlignmentOptions.Bottom);
 
+        /// <summary>
+        /// <see cref="IsOpen"/>
+        /// </summary>
         public static readonly BindableProperty IsOpenProperty = BindableProperty.Create(
             nameof(IsOpen),
             typeof(bool),
@@ -41,14 +50,23 @@ namespace DIPS.Xamarin.UI.Controls.Sheet
             BindingMode.TwoWay,
             propertyChanged: IsOpenPropertyChanged);
 
+        /// <summary>
+        /// <see cref="SheetContent"/>
+        /// </summary>
         public static readonly BindableProperty SheetContentProperty = BindableProperty.Create(nameof(SheetContent), typeof(View), typeof(SheetView), new ContentView(){HeightRequest = 100, VerticalOptions = LayoutOptions.Start});
 
+        /// <summary>
+        /// <see cref="BackgroundColor"/>
+        /// </summary>
         public static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create(
             nameof(BackgroundColor),
             typeof(Color),
             typeof(SheetView),
             Color.White);
 
+        /// <summary>
+        /// <see cref="Position"/>
+        /// </summary>
         public static readonly BindableProperty PositionProperty = BindableProperty.Create(
             nameof(Position),
             typeof(double),
@@ -56,100 +74,171 @@ namespace DIPS.Xamarin.UI.Controls.Sheet
             0.0,
             propertyChanged: OnPositionPropertyChanged);
 
+        /// <summary>
+        /// <see cref="MaxPosition"/>
+        /// </summary>
         public static readonly BindableProperty MaxPositionProperty = BindableProperty.Create(
             nameof(MaxPosition),
             typeof(double),
             typeof(SheetBehavior),
             1.0);
 
+        /// <summary>
+        /// <see cref="MinPosition"/>
+        /// </summary>
         public static readonly BindableProperty MinPositionProperty = BindableProperty.Create(
             nameof(MinPosition),
             typeof(double),
             typeof(SheetBehavior),
             0.1);
 
+        /// <summary>
+        /// <see cref="BindingContextFactory"/>
+        /// </summary>
         public static readonly BindableProperty BindingContextFactoryProperty = BindableProperty.Create(
             nameof(BindingContextFactory),
             typeof(Func<object>),
             typeof(SheetBehavior));
 
+        /// <summary>
+        /// <see cref="IsDraggable"/>
+        /// </summary>
         public static readonly BindableProperty IsDraggableProperty = BindableProperty.Create(
             nameof(IsDraggable),
             typeof(bool),
             typeof(SheetBehavior));
 
+        /// <summary>
+        /// <see cref="HasShadow"/>
+        /// </summary>
         public static readonly BindableProperty HasShadowProperty = BindableProperty.Create(nameof(HasShadow), typeof(bool), typeof(SheetBehavior), true);
 
+        /// <summary>
+        /// Determines if the sheet should have shadow.
+        /// This is a bindable property
+        /// </summary>
+        /// <remarks>This only works for iOS.</remarks>
         public bool HasShadow
         {
             get => (bool)GetValue(HasShadowProperty);
             set => SetValue(HasShadowProperty, value);
         }
 
+        /// <summary>
+        /// <see cref="HandleColor"/>
+        /// </summary>
         public static readonly BindableProperty HandleColorProperty = BindableProperty.Create(nameof(HandleColor), typeof(Color), typeof(SheetBehavior), ColorPalette.QuinaryAir);
 
+        /// <summary>
+        /// Determines the color of the handle in the sheet.
+        /// This is a bindable property.
+        /// </summary>
         public Color HandleColor
         {
             get => (Color)GetValue(HandleColorProperty);
             set => SetValue(HandleColorProperty, value);
         }
 
+        /// <summary>
+        /// Determines the position of the sheet when it appears.
+        /// This is a bindable property.
+        /// </summary>
         public AlignmentOptions Alignment
         {
             get => (AlignmentOptions)GetValue(AlignmentProperty);
             set => SetValue(AlignmentProperty, value);
         }
 
+        /// <summary>
+        /// Determines the color of the background of the sheet.
+        /// This is a bindable property.
+        /// </summary>
         public Color BackgroundColor
         {
             get => (Color)GetValue(BackgroundColorProperty);
             set => SetValue(BackgroundColorProperty, value);
         }
 
+        /// <summary>
+        /// Used to set the binding context of the content of the sheet when the sheet opens.
+        /// This is a bindable property.
+        /// </summary>
         public Func<object> BindingContextFactory
         {
             get => (Func<object>)GetValue(BindingContextFactoryProperty);
             set => SetValue(BindingContextFactoryProperty, value);
         }
 
+        /// <summary>
+        /// Determines if the sheet should be draggable or not.
+        /// This is a bindable property.
+        /// </summary>
         public bool IsDraggable
         {
             get => (bool)GetValue(IsDraggableProperty);
             set => SetValue(IsDraggableProperty, value);
         }
 
+        /// <summary>
+        /// Determines if the sheet should be visible or not.
+        /// This is a bindable property.
+        /// </summary>
         public bool IsOpen
         {
             get => (bool)GetValue(IsOpenProperty);
             set => SetValue(IsOpenProperty, value);
         }
 
-        //Double between 0.1 - 1.0
+        /// <summary>
+        /// Determines the maximum position of the sheet when it is visible.
+        /// This is a bindable property.
+        /// </summary>
+        /// <remarks>This will affect the size of the sheet if <see cref="Position"/> is set to 0</remarks>
+        /// <remarks>This will affect the people that are dragging the sheet</remarks>
+        /// <remarks>The value have to be between 0 and 1.0 (percentage of the screen)</remarks>
         public double MaxPosition
         {
             get => (double)GetValue(MaxPositionProperty);
             set => SetValue(MaxPositionProperty, value);
         }
 
-        //Double between 0.1 - 1.0
+        /// <summary>
+        /// Determines the minimum position of the sheet when it is visible.
+        /// This is a bindable property.
+        /// </summary>
+        /// <remarks>This will affect the size of the sheet if <see cref="Position"/> is set to 0</remarks>
+        /// <remarks>This will affect the people that are dragging the sheet</remarks>
+        /// <remarks>The value have to be between 0 and 1.0 (percentage of the screen)</remarks>
         public double MinPosition
         {
             get => (double)GetValue(MinPositionProperty);
             set => SetValue(MinPositionProperty, value);
         }
 
+        /// <summary>
+        /// Determines the position of the sheet when it is visible.
+        /// This is a bindable property.
+        /// <remarks>This will affect the size of the sheet if <see cref="Position"/> is set to 0</remarks>
+        /// <remarks>The value have to be between 0 and 1.0 (percentage of the screen)</remarks>
+        /// </summary>
         public double Position
         {
             get => (double)GetValue(PositionProperty);
             set => SetValue(PositionProperty, value);
         }
 
+        /// <summary>
+        /// The content of the sheet.
+        /// This is a bindable property.
+        /// <remarks><see cref="BindingContextFactory"/> to set the binding context when the sheet opens</remarks>
+        /// </summary>
         public View SheetContent
         {
             get => (View)GetValue(SheetContentProperty);
             set => SetValue(SheetContentProperty, value);
         }
 
+        /// <inheritdoc />
         public void Hide()
         {
             IsOpen = false;
@@ -322,15 +411,33 @@ namespace DIPS.Xamarin.UI.Controls.Sheet
         }
     }
 
+    /// <summary>
+    /// The positions of the sheet when it appears.
+    /// </summary>
     public enum AlignmentOptions
     {
+        /// <summary>
+        /// Positions the sheet at the bottom of the screen and will be draggable from bottom to top.
+        /// </summary>
         Bottom,
+        /// <summary>
+        /// Positions the sheet at the top of the screen and will be draggable from top to bottom.
+        /// </summary>
         Top
     }
 
+    /// <summary>
+    /// The alignment of the content of the sheet.
+    /// </summary>
     public enum ContentAlignment
     {
+        /// <summary>
+        /// The content will only use as much space as it needs.
+        /// </summary>
         Fit,
+        /// <summary>
+        /// The content will use the entire sheet as space.
+        /// </summary>
         Fill
     }
 }
