c9e97adfcd2e03dd8ebc3ebd4bee18b06a7a22e4
Håvard Moås <hmo@dips.no>
2020-01-24 15:54:00 +0100

    Fixed sizing of radiobuttongroup (#113)
    * Fixed sizing of radiobuttongroup
    
    * Added reproducable project
    
    * Updated sample

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github112/Github112.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github112
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Github112 : ContentPage
+    {
+        public Github112()
+        {
+            InitializeComponent();
+        }
+    }
+}
