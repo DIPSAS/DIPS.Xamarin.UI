ae4c12fb638dd1d81ba4855abd0fe6992fa280e0
Rizan Zaky <rizan149@gmail.com>
2020-02-13 17:07:07 +0530

    removed using partial class separation

--- src/DIPS.Xamarin.UI/Controls/Tag/Tag.cs
+++ src/DIPS.Xamarin.UI/Controls/Tag/Tag.cs
@@ -5,12 +5,170 @@ namespace DIPS.Xamarin.UI.Controls.Tag
     /// <summary>
     ///     A <c>Frame</c> wrapping a <c>Label</c> to allow setting border radius for the <c>Label</c>.
     /// </summary>
-    public partial class Tag : Frame
+    public class Tag : Frame
     {
+        /// <summary>
+        ///     Bindable property for <see cref="Text" />
+        /// </summary>
+        public static readonly BindableProperty TextProperty = Label.TextProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="TextColor" />
+        /// </summary>
+        public static readonly BindableProperty TextColorProperty = Label.TextColorProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="FontFamily" />
+        /// </summary>
+        public static readonly BindableProperty FontFamilyProperty = Label.FontFamilyProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="FontAttributes" />
+        /// </summary>
+        public static readonly BindableProperty FontAttributesProperty = Label.FontAttributesProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="FontSize" />
+        /// </summary>
+        public static readonly BindableProperty FontSizeProperty =
+            BindableProperty.Create(nameof(FontSize), typeof(double), typeof(Tag), -1.0);
+
+        /// <summary>
+        ///     Bindable property for <see cref="LineBreakMode" />
+        /// </summary>
+        public static readonly BindableProperty LineBreakModeProperty = Label.LineBreakModeProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="MaxLines" />
+        /// </summary>
+        public static readonly BindableProperty MaxLinesProperty = Label.MaxLinesProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="FormattedText" />
+        /// </summary>
+        public static readonly BindableProperty FormattedTextProperty = Label.FormattedTextProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="CharacterSpacing" />
+        /// </summary>
+        public static readonly BindableProperty CharacterSpacingProperty = Label.CharacterSpacingProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="VerticalTextAlignment" />
+        /// </summary>
+        public static readonly BindableProperty VerticalTextAlignmentProperty = Label.VerticalTextAlignmentProperty;
+
+        /// <summary>
+        ///     Bindable property for <see cref="HorizontalTextAlignment" />
+        /// </summary>
+        public static readonly BindableProperty HorizontalTextAlignmentProperty = Label.HorizontalTextAlignmentProperty;
+
         /// <inheritdoc />
         public Tag()
         {
             Initialize();
+        }
+
+        /// <summary>
+        ///     Gets or sets the text for the Tag. This is a bindable property.
+        ///     Setting Text to a non-null value will set the FormattedText property to null.
+        /// </summary>
+        public string Text
+        {
+            get => (string) GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the Color for the text of this Tag. This is a bindable property.
+        /// </summary>
+        public Color TextColor
+        {
+            get => (Color) GetValue(TextColorProperty);
+            set => SetValue(TextColorProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the font family to which the font for the Tag belongs. This is a bindable property.
+        /// </summary>
+        public string FontFamily
+        {
+            get => (string) GetValue(FontFamilyProperty);
+            set => SetValue(FontFamilyProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the value that indicates whether the font for the Tag is bold, italic, or neither. This is a bindable
+        ///     property.
+        /// </summary>
+        public FontAttributes FontAttributes
+        {
+            get => (FontAttributes) GetValue(FontAttributesProperty);
+            set => SetValue(FontAttributesProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the size of the font for the Tag. This is a bindable property.
+        /// </summary>
+        public double FontSize
+        {
+            get => (double) GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the LineBreakMode for the Tag. This is a bindable property.
+        /// </summary>
+        public LineBreakMode LineBreakMode
+        {
+            get => (LineBreakMode) GetValue(LineBreakModeProperty);
+            set => SetValue(LineBreakModeProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the maximum number of lines allowed in the Tag.
+        /// </summary>
+        public int MaxLines
+        {
+            get => (int) GetValue(MaxLinesProperty);
+            set => SetValue(MaxLinesProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the formatted text for the Tag. This is a bindable property.
+        ///     Setting FormattedText to a non-null value will set the Text property to null.
+        /// </summary>
+        public FormattedString FormattedText
+        {
+            get => (FormattedString) GetValue(FormattedTextProperty);
+            set => SetValue(FormattedTextProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the character spacing of the text in the Tag. This is a bindable property.
+        /// </summary>
+        public double CharacterSpacing
+        {
+            get => (double) GetValue(CharacterSpacingProperty);
+            set => SetValue(CharacterSpacingProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the vertical alignment of the Text property. This is a bindable property.
+        /// </summary>
+        public TextAlignment VerticalTextAlignment
+        {
+            get => (TextAlignment) GetValue(VerticalTextAlignmentProperty);
+            set => SetValue(VerticalTextAlignmentProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the horizontal alignment of the Text property. This is a bindable property.
+        /// </summary>
+        public TextAlignment HorizontalTextAlignment
+        {
+            get => (TextAlignment) GetValue(HorizontalTextAlignmentProperty);
+            set => SetValue(HorizontalTextAlignmentProperty, value);
         }
 
         private void Initialize()
