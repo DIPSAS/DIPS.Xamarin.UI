8a6665ba014dbf422161ef48c493d1a1ee9f22fb
Erik Kvanli <0erikkvanli0@gmail.com>
2020-03-10 19:30:01 +0100

    Initial work on new samples app

---
+++ src/Samples/DIPS.Xamarin.UI.Samples/View/SamplesButton.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DIPS.Xamarin.UI.Samples.View
+{
+    public partial class SamplesButton : ContentView
+    {
+        public SamplesButton()
+        {
+            InitializeComponent();
+            BindingContextChanged += SamplesButton_BindingContextChanged;
+        }
+
+        private void SamplesButton_BindingContextChanged(object sender, EventArgs e)
+        {
+            if (BindingContext == null) return;
+            btn.Text = Text;
+            btn.CommandParameter = CommandParameter;
+            img.Source = Image;
+        }
+
+
+        public string Text { get; set; } = string.Empty;
+        public ImageSource Image { get; set; } = string.Empty;
+        public object? CommandParameter { get; set; }
+
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create(
+            nameof(Command),
+            typeof(Command),
+            typeof(SamplesButton));
+
+        public Command Command
+        {
+            get => (Command)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+    }
+}
