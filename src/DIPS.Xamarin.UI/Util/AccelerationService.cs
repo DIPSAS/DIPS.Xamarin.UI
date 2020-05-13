5dcf6559796df7c59f8370c5ebc44aec45899014
Erik Kvanli <ekv@dips.no>
2020-05-13 12:38:57 +0200

    Fixed acceleration by time and added tap to stop (#174)
    SlidableLayout

--- src/DIPS.Xamarin.UI/Util/AccelerationService.cs
+++ src/DIPS.Xamarin.UI/Util/AccelerationService.cs
@@ -8,7 +8,7 @@ namespace DIPS.Xamarin.UI.Util
     /// </summary>
     internal class AccelerationService
     {
-        private const double DefaultFriction = 0.2, DefaultGravity = 10.0, DefaultTrackTime = 0.18, ErrorMargin = 0.01;
+        private const double DefaultFriction = 2, DefaultGravity = 10.0, DefaultTrackTime = 0.18, ErrorMargin = 0.05;
         private readonly TimeTracker m_timeTracker = new TimeTracker();
 
         private readonly Snapper? m_snapper;
@@ -125,6 +125,7 @@ namespace DIPS.Xamarin.UI.Util
                 }
 
                 if (i < 0) i = 0;
+                if (time < 0.1) time = 0.1;
                 m_speed = (m_moves[m_moves.Count - 1].Item1 - m_moves[i].Item1) / time;
             }
         }
@@ -144,7 +145,7 @@ namespace DIPS.Xamarin.UI.Util
                     return m_value;
                 }
 
-                var time = m_timeTracker.GetTimeD();
+                var time = Math.Min(m_timeTracker.GetTimeD(), 1.0);
                 if (m_snapper == null)
                 {
                     ApplyFriction(time);
@@ -185,7 +186,7 @@ namespace DIPS.Xamarin.UI.Util
             }
         }
 
-        private bool IsDoneWithoutSnap() => Math.Abs(m_speed) < 1;
+        private bool IsDoneWithoutSnap() => Math.Abs(m_speed) < m_gravity / 3.0;
 
         private bool IsDone(double snapPoint) => Math.Abs(snapPoint - m_value) < ErrorMargin && IsDoneWithoutSnap();
 
@@ -196,7 +197,8 @@ namespace DIPS.Xamarin.UI.Util
 
         private void ApplyFriction(double time)
         {
-            m_speed -= m_speed * m_friction;
+            var sign = Math.Sign(m_speed);
+            m_speed -= Math.Max(-0.1 * sign, m_speed * m_friction * time * sign) * sign;
         }
 
         private void ApplyGravity(double time, double snapPoint)
