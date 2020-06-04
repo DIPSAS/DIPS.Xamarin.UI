448c8ef042012413669ccd5e5c2a78fcbbff2210
Håvard Moås <hmo@dips.no>
2020-06-04 21:49:03 +0200

    Added StringCase markup extension + tests (#181)

---
+++ src/Samples/DIPS.Xamarin.UI.Samples/Converters/ValueConverters/StringCaseConverterPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.UI.Samples.Converters.ValueConverters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StringCaseConverterPage : ContentPage
+    {
+        public StringCaseConverterPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class StringCaseConverterViewModel
+    {
+        public string MyString => "Test string";
+    }
+}
