493cda8493233cf62ecedcded01376fe2c32aa2f
Håvard Moås <hmo@dips.no>
2020-06-15 22:26:21 +0200

    Made sure that IsVisible is respected when floating action menu tries to open/close

--- src/DIPS.Xamarin.UI/Internal/Xaml/FloatingActionMenu.xaml.cs
+++ src/DIPS.Xamarin.UI/Internal/Xaml/FloatingActionMenu.xaml.cs
@@ -97,13 +97,16 @@ namespace DIPS.Xamarin.UI.Internal.Xaml
             var multiplier = 1;
             foreach (var menuButton in Children)
             {
-                var maxOpacity = menuButton.IsEnabled ? 1 : .5;
-                menuButton.TranslateTo(0, m_isExpanded ? 0 : -m_yTranslate * multiplier, 250, Easing.CubicInOut);
-                menuButton.Button.FadeTo(m_isExpanded ? 0 : maxOpacity, 250, Easing.CubicInOut);
-                menuButton.TitleFrame.FadeTo(m_isExpanded ? 0 : 1, 250, Easing.CubicInOut);
-                menuButton.BadgeFrame.FadeTo(m_isExpanded ? .5 : .95, 250, Easing.CubicInOut);
+                if(menuButton.IsVisible)
+                {
+                    var maxOpacity = menuButton.IsEnabled ? 1 : .5;
+                    menuButton.TranslateTo(0, m_isExpanded ? 0 : -m_yTranslate * multiplier, 250, Easing.CubicInOut);
+                    menuButton.Button.FadeTo(m_isExpanded ? 0 : maxOpacity, 250, Easing.CubicInOut);
+                    menuButton.TitleFrame.FadeTo(m_isExpanded ? 0 : 1, 250, Easing.CubicInOut);
+                    menuButton.BadgeFrame.FadeTo(m_isExpanded ? .5 : .95, 250, Easing.CubicInOut);
 
-                multiplier += 1;
+                    multiplier += 1;
+                }
             }
 
             if (m_isExpanded)
