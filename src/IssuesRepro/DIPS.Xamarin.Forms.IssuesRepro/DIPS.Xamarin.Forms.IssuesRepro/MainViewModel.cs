1f76c88f4bd9271b0ab2dff51cc4a49e680e2f22
Håvard Moås <hmo@dips.no>
2020-06-16 10:47:58 +0200

    Made sure that IsVisible is respected when floating action menu tries to open/close (#186)

--- src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/MainViewModel.cs
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Input;
+using DIPS.Xamarin.Forms.IssuesRepro.HideMenuButton;
 using Xamarin.Forms;
 
 namespace DIPS.Xamarin.Forms.IssuesRepro
@@ -30,6 +31,9 @@ namespace DIPS.Xamarin.Forms.IssuesRepro
                 Application.Current.MainPage.Navigation.PushAsync(new Github150.Github150());
             if (obj.Equals("159"))
                 Application.Current.MainPage.Navigation.PushAsync(new Github159.Github159());
+            if (obj.Equals("185"))
+                Application.Current.MainPage.Navigation.PushAsync(new Github185Page());
+            ;
         }
     }
 }
