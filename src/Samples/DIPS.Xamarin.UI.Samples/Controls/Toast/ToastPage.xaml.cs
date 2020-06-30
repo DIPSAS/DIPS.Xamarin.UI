7cb47f3bb583ebe9d9ab27890937929358b9d9ee
Rizan Zaky <rizan149@gmail.com>
2020-07-01 00:50:55 +0530

    toast action sample

--- src/Samples/DIPS.Xamarin.UI.Samples/Controls/Toast/ToastPage.xaml.cs
+++ src/Samples/DIPS.Xamarin.UI.Samples/Controls/Toast/ToastPage.xaml.cs
@@ -12,6 +12,7 @@ namespace DIPS.Xamarin.UI.Samples.Controls.Toast
     public partial class ToastPage : ContentPage
     {
         private ICommand m_showToastCommand;
+        private ICommand m_showToastCommand2;
 
         public ToastPage()
         {
@@ -21,6 +22,19 @@ namespace DIPS.Xamarin.UI.Samples.Controls.Toast
 
             ShowToastCommand = new Command(parameter =>
             {
+                Toaster.Current.ToastAction = async () =>
+                {
+                    await Task.Delay(2000);
+                    Console.WriteLine("Hello, Jupiter!");
+                };
+                Toaster.Current.Text = parameter.ToString();
+
+                Toaster.Current.ShowToaster();
+            });
+
+            ShowToastCommand2 = new Command(parameter =>
+            {
+                Toaster.Current.ToastAction = null;
                 Toaster.Current.Text = parameter.ToString();
 
                 Toaster.Current.ShowToaster();
@@ -37,6 +51,16 @@ namespace DIPS.Xamarin.UI.Samples.Controls.Toast
             }
         }
 
+        public ICommand ShowToastCommand2
+        {
+            get => m_showToastCommand2;
+            set
+            {
+                m_showToastCommand2 = value;
+                OnPropertyChanged(nameof(ShowToastCommand2));
+            }
+        }
+
         private static void ConfigureToast()
         {
             Toaster.Current.FontSize = 13;
@@ -47,11 +71,6 @@ namespace DIPS.Xamarin.UI.Samples.Controls.Toast
             Toaster.Current.PositionY = 30;
             Toaster.Current.AnimateFor = 500;
             Toaster.Current.HideToastIn = 5;
-            Toaster.Current.ToastAction = async () =>
-            {
-                await Task.Delay(2000);
-                Console.WriteLine("Hello, Jupiter!");
-            };
         }
     }
 }
