890f274834d2f77dc49064ae5f25241587272a7c
Håvard Moås <hmo@dips.no>
2020-06-30 10:54:57 +0200

    Added LogicalExpressionConverter (#193)
    * Bumped Xamarin.Forms and created MultiConverters folder
    
    * Bumped Xam.Forms 4.7.0
    
    Added LogicalExpressionConverter
    
    Made sure Sheet title bar is visible for all potential cases where it should be visible
    
    * Added missing comments
    
    Co-authored-by: Erik Kvanli <ekv@dips.no>

---
+++ src/DIPS.Xamarin.UI/Converters/MultiValueConverters/LogicalExpressionConverter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using DIPS.Xamarin.UI.Internal.Utilities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.UI.Converters.MultiValueConverters
+{
+    /// <summary>
+    /// A converter to run a logical gate on multiple boolean values <see cref="LogicalGate"/>.
+    /// </summary>
+    public class LogicalExpressionConverter : IMarkupExtension, IMultiValueConverter
+    {
+        private IServiceProvider? m_serviceProvider;
+
+        /// <summary>
+        /// <inheritdoc cref="LogicalGate"/>
+        /// </summary>
+        public LogicalGate LogicalGate { get; set; }
+        /// <summary>
+        /// A object to return when the converter returns true
+        /// </summary>
+        public object TrueObject { get; set; } = true;
+        /// <summary>
+        /// A object to return when the converter returns false
+        /// </summary>
+        public object FalseObject { get; set; } = false;
+        /// <summary>
+        /// A boolean value to tell the converter to invert the output
+        /// </summary>
+        public bool Inverted { get; set; }
+
+        /// <inheritdoc/>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null)
+            {
+                return FalseObject;
+            }
+
+            if (values.Any(o => o == null))
+                return FalseObject;
+
+            try
+            {
+                var bools = values.Cast<bool>().ToList();
+
+                var logcalExpression = false;
+                logcalExpression = LogicalGate switch
+                {
+                    LogicalGate.And => bools.All(b => b),
+                    LogicalGate.Nand => !bools.All(b => b),
+                    LogicalGate.Or => bools.Any(b => b),
+                    LogicalGate.Nor => !bools.Any(b => b),
+                    LogicalGate.Xor => bools.Count(b => b) == 1,
+                    LogicalGate.Xand => bools.All(b => b) || bools.All(b => !b),
+                    _ => throw new ArgumentOutOfRangeException(),
+                };
+                return logcalExpression ? !Inverted ? TrueObject : FalseObject : !Inverted ? FalseObject : TrueObject;
+
+            }
+            catch (Exception e)
+            {
+                throw new XamlParseException($"LogicalExpressionConverter : Something went wrong while converting: {e.Message}").WithXmlLineInfo(m_serviceProvider);
+            }
+        }
+
+        /// <inheritdoc/>
+        [ExcludeFromCodeCoverage]
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("LogicalExpressionConverter does not support convert back");
+        }
+
+        /// <inheritdoc/>
+        [ExcludeFromCodeCoverage]
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            m_serviceProvider = serviceProvider;
+            return this;
+        }
+    }
+
+    /// <summary>
+    /// A logical gate
+    /// </summary>
+    public enum LogicalGate
+    {
+        /// <summary>
+        /// And <see href="https://en.wikipedia.org/wiki/NAND_gate"/>
+        /// </summary>
+        And,
+        /// <summary>
+        /// Nand <see href="https://en.wikipedia.org/wiki/NAND_gate"/>
+        /// </summary>
+        Nand,
+        /// <summary>
+        /// Or <see href="https://en.wikipedia.org/wiki/OR_gate"/>
+        /// </summary>
+        Or,
+        /// <summary>
+        /// Nor <see href="https://en.wikipedia.org/wiki/NOR_gate"/>
+        /// </summary>
+        Nor,
+        /// <summary>
+        ///Exclusive Or <see href="https://en.wikipedia.org/wiki/Exclusive_or"/>
+        /// </summary>
+        Xor,
+        /// <summary>
+        /// Exclusive And <see href="https://deepai.org/machine-learning-glossary-and-terms/xand"/>
+        /// </summary>
+        Xand,
+    }
+}
---
+++ src/Tests/DIPS.Xamarin.UI.Tests/Converters/MultiValueConverters/LogicalExpressionConverterTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DIPS.Xamarin.UI.Converters.MultiValueConverters;
+using FluentAssertions;
+using Xunit;
+using DIPS.Xamarin.UI.Tests.TestHelpers;
+using Xamarin.Forms;
+
+namespace DIPS.Xamarin.UI.Tests.Converters.MultiValueConverters
+{
+    public class LogicalExpressionConverterTests
+    {
+        private LogicalExpressionConverter m_logicalExpressionConverter;
+
+        public LogicalExpressionConverterTests()
+        {
+            m_logicalExpressionConverter = new LogicalExpressionConverter();
+        }
+
+
+        [Fact]
+        public void Convert_ValuesAreNull_ReturnsFalse()
+        {
+            var output = m_logicalExpressionConverter.Convert<bool>(null);
+
+            output.Should().BeFalse(because: "empty input can not be converted and converter should return false");
+        }
+
+
+        [Fact]
+        public void Convert_AllValuesInInputAreNull_ReturnsFalse()
+        {
+            var output = m_logicalExpressionConverter.Convert<bool>(new object[] { null, null });
+
+            output.Should().BeFalse(because: "if all values are null it can not be converted and converter should return false");
+        }
+
+        [Fact]
+        public void Convert_AnyValuesInInputAreNull_ReturnsFalse()
+        {
+            var output = m_logicalExpressionConverter.Convert<bool>(new object[] { true, null });
+
+            output.Should().BeFalse("if any values are null it can not be converted and converter should return false");
+        }
+
+        [Theory]
+        [InlineData(new object[] { new object[] { false, false }, false })]
+        [InlineData(new object[] { new object[] { true, false}, false})]
+        [InlineData(new object[] { new object[] { false, true}, false})]
+        [InlineData(new object[] { new object[] { true, true}, true})]
+        public void Convert_And_ShouldReturnAsExpected(object[] inputs, bool expectedOutput)
+        {
+            m_logicalExpressionConverter.LogicalGate = LogicalGate.And;
+            var output = m_logicalExpressionConverter.Convert<bool>(inputs);
+
+            output.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(new object[] { new object[] { false, false }, true })]
+        [InlineData(new object[] { new object[] { true, false }, true })]
+        [InlineData(new object[] { new object[] { false, true }, true })]
+        [InlineData(new object[] { new object[] { true, true }, false })]
+        public void Convert_Nand_ShouldReturnAsExpected(object[] inputs, bool expectedOutput)
+        {
+            m_logicalExpressionConverter.LogicalGate = LogicalGate.Nand;
+            var output = m_logicalExpressionConverter.Convert<bool>(inputs);
+
+            output.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(new object[] { new object[] { false, false }, true })]
+        [InlineData(new object[] { new object[] { true, false }, false })]
+        [InlineData(new object[] { new object[] { false, true }, false })]
+        [InlineData(new object[] { new object[] { true, true }, true })]
+        public void Convert_Xand_ShouldReturnAsExpected(object[] inputs, bool expectedOutput)
+        {
+            m_logicalExpressionConverter.LogicalGate = LogicalGate.Xand;
+            var output = m_logicalExpressionConverter.Convert<bool>(inputs);
+
+            output.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(new object[] { new object[] { false, false }, false })]
+        [InlineData(new object[] { new object[] { true, false }, true })]
+        [InlineData(new object[] { new object[] { false, true }, true })]
+        [InlineData(new object[] { new object[] { true, true }, true })]
+        public void Convert_Or_ShouldReturnAsExpected(object[] inputs, bool expectedOutput)
+        {
+            m_logicalExpressionConverter.LogicalGate = LogicalGate.Or;
+            var output = m_logicalExpressionConverter.Convert<bool>(inputs);
+
+            output.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(new object[] { new object[] { false, false }, true })]
+        [InlineData(new object[] { new object[] { true, false }, false })]
+        [InlineData(new object[] { new object[] { false, true }, false })]
+        [InlineData(new object[] { new object[] { true, true }, false })]
+        public void Convert_Nor_ShouldReturnAsExpected(object[] inputs, bool expectedOutput)
+        {
+            m_logicalExpressionConverter.LogicalGate = LogicalGate.Nor;
+            var output = m_logicalExpressionConverter.Convert<bool>(inputs);
+
+            output.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(new object[] { new object[] { false, false }, false })]
+        [InlineData(new object[] { new object[] { true, false }, true })]
+        [InlineData(new object[] { new object[] { false, true }, true })]
+        [InlineData(new object[] { new object[] { true, true }, false })]
+        public void Convert_Xor_ShouldReturnAsExpected(object[] inputs, bool expectedOutput)
+        {
+            m_logicalExpressionConverter.LogicalGate = LogicalGate.Xor;
+            var output = m_logicalExpressionConverter.Convert<bool>(inputs);
+
+            output.Should().Be(expectedOutput);
+        }
+
+    }
+}
