271eb6e4692034218d26abcf487174f1af8f3264
Rizan Zaky <rizan149@gmail.com>
2020-07-01 00:03:19 +0530

    organize sample

--- src/Samples/DIPS.Xamarin.UI.Samples/Controls/Toast/ToastPage.xaml.cs
+++ src/Samples/DIPS.Xamarin.UI.Samples/Controls/Toast/ToastPage.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using DIPS.Xamarin.UI.Controls.Toast;
 using Xamarin.Forms;
@@ -14,60 +9,42 @@ namespace DIPS.Xamarin.UI.Samples.Controls.Toast
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ToastPage : ContentPage
     {
+        private ICommand m_showToastCommand;
+
         public ToastPage()
         {
             InitializeComponent();
-            
-            ShowToastCommand = new Command(() =>
-            {
-                // var toaster = new Toaster();
-                // toaster.ShowToast();
 
-                Toaster.Current.Text = "Hello, Jupiter!";
-                Toaster.Current.FontSize = 13;
-                Toaster.Current.TextColor = Color.White;
-                Toaster.Current.BackgroundColor = Color.DodgerBlue;
-                Toaster.Current.CornerRadius = 8;
-                Toaster.Current.Padding = new Thickness(20, 10);
-                Toaster.Current.PositionY = 30;
-                Toaster.Current.AnimateFor = 500;
-                Toaster.Current.HideToastIn = 5;
-                
+            ConfigureToast();
+
+            ShowToastCommand = new Command(parameter =>
+            {
+                Toaster.Current.Text = parameter.ToString();
+
                 Toaster.Current.ShowToaster();
             });
         }
-        
-        void OnButtonClicked(object sender, EventArgs args)
-        {
-            // var toaster = new Toaster();
-            // toaster.ShowToast();
 
-            Toaster.Current.Text = "Hello, Jupiter!";
+        public ICommand ShowToastCommand
+        {
+            get => m_showToastCommand;
+            set
+            {
+                m_showToastCommand = value;
+                OnPropertyChanged(nameof(ShowToastCommand));
+            }
+        }
+
+        private static void ConfigureToast()
+        {
             Toaster.Current.FontSize = 13;
             Toaster.Current.TextColor = Color.White;
             Toaster.Current.BackgroundColor = Color.DodgerBlue;
             Toaster.Current.CornerRadius = 8;
             Toaster.Current.Padding = new Thickness(20, 10);
             Toaster.Current.PositionY = 30;
-            
-            Toaster.Current.ShowToaster();
-        }
-
-        private ICommand _showToastCommand;
-
-        public ICommand ShowToastCommand
-        {
-            get => _showToastCommand;
-            set
-            {
-                _showToastCommand = value;
-                OnPropertyChanged(nameof(ShowToastCommand));
-            }
-        }
-
-        private void OnMyButtonClicked(object sender, EventArgs e)
-        {
-            Console.WriteLine("Hellooo...");
+            Toaster.Current.AnimateFor = 500;
+            Toaster.Current.HideToastIn = 5;
         }
     }
 }
