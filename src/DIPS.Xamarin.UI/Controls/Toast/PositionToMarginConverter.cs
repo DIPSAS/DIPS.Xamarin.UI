209a8ec979709d5cc74cce1665cff7f73f7f9887
Rizan Zaky <rizan149@gmail.com>
2020-07-01 00:55:24 +0530

    cleanups

--- src/DIPS.Xamarin.UI/Controls/Toast/PositionToMarginConverter.cs
+++ src/DIPS.Xamarin.UI/Controls/Toast/PositionToMarginConverter.cs
@@ -8,7 +8,7 @@ namespace DIPS.Xamarin.UI.Controls.Toast
     public class PositionToMarginConverter : IMarkupExtension, IValueConverter
     {
         private IServiceProvider m_serviceProvider;
-        
+
         public object ProvideValue(IServiceProvider serviceProvider)
         {
             m_serviceProvider = serviceProvider;
@@ -19,22 +19,16 @@ namespace DIPS.Xamarin.UI.Controls.Toast
         {
             if (value is double positionY)
             {
-                var ratio = positionY < 0 ? 0 : (positionY > 100 ? 100 : positionY);
-                var size = ratio * (Application.Current.MainPage.Height - 60) / 100;
+                var ratio = positionY < 0 ? 0 : positionY > 100 ? 100 : positionY;
+                var size = (ratio * (Application.Current.MainPage.Height - 60)) / 100;
                 return new Thickness(0, size, 0, 0);
             }
-            
+
             return new Thickness(0, 10, 0, 0);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            
-            // var margin = (Thickness)GetValue(PositionYProperty);
-            // return margin.Top * 100 / Application.Current.MainPage.Height;
-            // var ratio = margin.Top < 0 ? 0 : (margin.Top > 100 ? 100 : margin.Top);
-            // var size = ratio * Application.Current.MainPage.Height / 100;
-            // return margin.Top;
             throw new NotImplementedException();
         }
     }
