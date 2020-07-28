7801295bba8715c31cf97f9a3606f37962a525f2
Erik Kvanli <0erikkvanli0@gmail.com>
2020-07-28 11:30:21 +0200

    Added DateSelected event to the picker.
    Solves #203

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github203/Github203.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github203
+{
+    public partial class Github203 : ContentPage
+    {
+        public Github203()
+        {
+            InitializeComponent();
+        }
+
+        public void DateChanged(object sender, DateChangedEventArgs eventArgs)
+        {
+            text.Text = eventArgs.NewDate.ToString();
+        }
+    }
+}
