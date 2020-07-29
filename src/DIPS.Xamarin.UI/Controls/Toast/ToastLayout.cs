862f092add23932af0c6074384a2857f059a73a3
Rizan Zaky <rizan149@gmail.com>
2020-07-29 17:15:03 +0530

    add default values

--- src/DIPS.Xamarin.UI/Controls/Toast/ToastLayout.cs
+++ src/DIPS.Xamarin.UI/Controls/Toast/ToastLayout.cs
@@ -9,13 +9,15 @@ namespace DIPS.Xamarin.UI.Controls.Toast
     {
         /// <summary>
         ///     Gets or sets the color which will fill the background of the Toast.
+        ///     <remarks>Default value is <see cref="Color.Black" /></remarks>
         /// </summary>
-        public Color BackgroundColor { get; set; }
+        public Color BackgroundColor { get; set; } = Color.Black;
 
         /// <summary>
         ///     Gets or sets the corner radius of the Toast.
+        ///     <remarks>Default value is 8</remarks>
         /// </summary>
-        public float CornerRadius { get; set; }
+        public float CornerRadius { get; set; } = 8;
 
         /// <summary>
         ///     Gets or sets the font family to which the font for the Toast belongs.
@@ -24,14 +26,15 @@ namespace DIPS.Xamarin.UI.Controls.Toast
 
         /// <summary>
         ///     Gets or sets the size of the font for the Toast.
+        ///     <remarks>Default value is 11</remarks>
         /// </summary>
-        [TypeConverter(typeof(FontSizeConverter))]
-        public double FontSize { get; set; }
+        public double FontSize { get; set; } = 11;
 
         /// <summary>
         ///     Gets or sets the LineBreakMode for the Toast.
+        ///     <remarks>Default value is <see cref="LineBreakMode.TailTruncation" /></remarks>
         /// </summary>
-        public LineBreakMode LineBreakMode { get; set; }
+        public LineBreakMode LineBreakMode { get; set; } = LineBreakMode.TailTruncation;
 
         /// <summary>
         ///     Gets or sets a flag indicating if the Toast has a shadow displayed.
@@ -40,27 +43,31 @@ namespace DIPS.Xamarin.UI.Controls.Toast
 
         /// <summary>
         ///     Gets or sets the maximum number of lines allowed in the Toast.
+        ///     <remarks>Default value is 1</remarks>
         /// </summary>
-        public int MaxLines { get; set; }
+        public int MaxLines { get; set; } = 1;
 
         /// <summary>
         ///     Gets or sets the inner padding of the Toast text.
         ///     <remarks>
         ///         The padding is the space between the bounds of a Toast and the bounding region into which its Text property
         ///         should be arranged into.
+        ///         Default value is 8 on all sides"/>
         ///     </remarks>
         /// </summary>
-        public Thickness Padding { get; set; }
+        public Thickness Padding { get; set; } = new Thickness(8);
 
         /// <summary>
         ///     The vertical positioning of the toast in a percentage of the Main Page relative to the top margin of the Main
         ///     page.
+        ///     <remarks>Default value is 1% off from the Navigation Bar</remarks>
         /// </summary>
-        public double PositionY { get; set; }
+        public double PositionY { get; set; } = 1;
 
         /// <summary>
         ///     Gets or sets the Color for the text of this Toast.
+        ///     <remarks>Default value is <see cref="Color.White" /></remarks>
         /// </summary>
-        public Color TextColor { get; set; }
+        public Color TextColor { get; set; } = Color.White;
     }
 }
--- src/DIPS.Xamarin.UI/Controls/Toast/ToastOptions.cs
+++ src/DIPS.Xamarin.UI/Controls/Toast/ToastOptions.cs
@@ -8,24 +8,31 @@ namespace DIPS.Xamarin.UI.Controls.Toast
     {
         /// <summary>
         ///     Performs action on tapping the toast
-        ///     <remarks> Will Override closing the toast on tapping </remarks>
+        ///     <remarks>Will Override closing the toast on tapping</remarks>
         /// </summary>
         public Action? ToastAction { get; set; }
 
         /// <summary>
-        ///     Animation on displaying the Toast
+        ///     Animation on displaying the Toast.
+        ///     <remarks>Default animation is Fading-In in 250 ms</remarks>
         /// </summary>
-        public Func<ToastView, Task> DisplayAnimation { get; set; }
+        public Func<ToastView, Task> DisplayAnimation { get; set; } = toastView =>
+        {
+            toastView.Opacity = 0;
+            return toastView.FadeTo(1, 500, Easing.Linear);
+        };
 
         /// <summary>
         ///     Animation on closing the Toast
+        ///     <remarks>Default animation is Fading-Out in 250 ms</remarks>
         /// </summary>
-        public Func<ToastView, Task> CloseAnimation { get; set; }
+        public Func<ToastView, Task> CloseAnimation { get; set; } =
+            toastView => toastView.FadeTo(0, 500, Easing.Linear);
 
         /// <summary>
         ///     Hide the toast automatically after the given milliseconds
-        ///     <remarks> If value is 0, toast won't be hidden automatically </remarks>
+        ///     <remarks>If value is 0, toast won't be hidden automatically. Default value is 3000 ms</remarks>
         /// </summary>
-        public int HideToastIn { get; set; }
+        public int HideToastIn { get; set; } = 3000;
     }
 }
