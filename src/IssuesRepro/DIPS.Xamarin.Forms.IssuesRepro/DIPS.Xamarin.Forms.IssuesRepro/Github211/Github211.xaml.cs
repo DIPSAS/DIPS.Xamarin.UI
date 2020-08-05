6f2b7f35cefcc54c0d029f1bbb918fd9f722a18e
Erik Kvanli <0erikkvanli0@gmail.com>
2020-08-05 13:45:01 +0200

    Remove crashing and cleaned up issue client

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github211/Github211.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using DIPS.Xamarin.UI.Extensions;
+using Xamarin.Forms;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github211
+{
+    public partial class Github211 : ContentPage
+    {
+        public Github211()
+        {
+            var vm = new Issue211ViewModel();
+            vm.Initialize();
+            BindingContext = vm;
+            InitializeComponent();
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            skeleton.MainContent = new Label { Text = "Zup?" };
+        }
+    }
+
+    public class Issue211ViewModel : INotifyPropertyChanged
+    {
+        private static readonly Random s_rnd = new Random();
+        private bool m_isLoading = false;
+
+        public bool IsLoading { get => m_isLoading; set => PropertyChanged.RaiseWhenSet(ref m_isLoading, value); } 
+        public async void Initialize()
+        {
+            while (true)
+            {
+                IsLoading = !IsLoading;
+                await Task.Delay(s_rnd.Next(100, 2000));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/MainViewModel.cs
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/MainViewModel.cs
@@ -1,44 +1,43 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace DIPS.Xamarin.Forms.IssuesRepro
 {
     public class MainViewModel
     {
-        public MainViewModel()
+        private static readonly Dictionary<string, Func<Page>> s_issues = new Dictionary<string, Func<Page>>
         {
-            NavigationCommand = new Command<string>(Navigate);
-        }
+            { "64", () => new Github64.Github64() },
+            { "86", () => new Github86.Github86() },
+            { "112", () => new Github112.Github112() },
+            { "120", () => new Github120.Github120Page() },
+            { "123", () => new Github123.Github123() },
+            { "137", () => new NewIssue.Github137() },
+            { "142", () => new Github142.Github142() },
+            { "150", () => new Github150.Github150() },
+            { "159", () => new Github159.Github159() },
+            { "179", () => new Github179.Github179() },
+            { "185", () => new Github185.Github185Page() },
+            { "203", () => new Github203.Github203() },
+            { "211", () => new Github211.Github211() },
+        };
 
-        public ICommand NavigationCommand { get; }
+        public List<IssueViewModel> Issues { get; } = s_issues.Keys.Select(l => new IssueViewModel(l)).ToList();
 
-        private void Navigate(string obj)
+        public class IssueViewModel
         {
-            if (obj.Equals("64"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github64.Github64());
-            if (obj.Equals("86"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github86.Github86());
-            if (obj.Equals("112"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github112.Github112());
-            if (obj.Equals("123"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github123.Github123());
-            if (obj.Equals("137"))
-                Application.Current.MainPage.Navigation.PushAsync(new NewIssue.Github137());
-            if (obj.Equals("142"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github142.Github142());
-            if (obj.Equals("150"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github150.Github150());
-            if (obj.Equals("159"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github159.Github159());
-            if (obj.Equals("179"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github179.Github179());
-            if (obj.Equals("185"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github185.Github185Page());
-            if (obj.Equals("120"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github120.Github120Page());
-            if (obj.Equals("203"))
-                Application.Current.MainPage.Navigation.PushAsync(new Github203.Github203());
-            ;
+            public IssueViewModel(string value)
+            {
+                Value = value;
+                OnTapCommand = new Command(() => Application.Current.MainPage.Navigation.PushAsync(s_issues[value]()));
+            }
+
+            public ICommand OnTapCommand { get; }
+            public string Value { get; }
+            public string PresentedValue => "Github issue " + Value;
         }
     }
 }
