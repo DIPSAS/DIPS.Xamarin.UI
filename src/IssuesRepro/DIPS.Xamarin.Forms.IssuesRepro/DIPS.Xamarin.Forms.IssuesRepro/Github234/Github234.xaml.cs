ee521e7899f4e34560d04eb7749bcf69627913ca
Erik Kvanli <ekv@dips.no>
2020-09-14 12:41:14 +0200

    Bug/ekv 234 (#235)
    Fixed crashing floating button

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github234/Github234.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github234
+{
+    [Issue(234)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Github234 : ContentPage
+    {
+        public static Page CurrentMain;
+        public static bool StopIt;
+        public Github234()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (!StopIt)
+            {
+                Button_Clicked2(null, EventArgs.Empty);
+            }
+        }
+
+        async void Button_Clicked2(System.Object sender, System.EventArgs e)
+        {
+            CurrentMain = Application.Current.MainPage;
+            try
+            {
+                var p = await Task.Run(async () =>
+                {
+                    Thread.Sleep(10);
+                    var page = new Github234FloatingButtonPage();
+                    return page;
+                });
+                Application.Current.MainPage = p;
+            }
+            catch(Exception exception)
+            {
+                Application.Current.MainPage = new ContentPage
+                {
+                    Content = new Label
+                    {
+                        Text = exception.Message + "\n" + exception.StackTrace,
+                    }
+                };
+            }
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            StopIt = !StopIt;
+        }
+    }
+}
