fd9547fbf651bd32ec7c2e7fa9338445068d46e0
Erling Moxnes Kristiansen <erling.m.kristiansen@gmail.com>
2020-10-21 14:54:32 +0200

    Update RadioButton.xaml.cs

--- src/DIPS.Xamarin.UI/Internal/Xaml/RadioButton.xaml.cs
+++ src/DIPS.Xamarin.UI/Internal/Xaml/RadioButton.xaml.cs
@@ -128,8 +128,16 @@ namespace DIPS.Xamarin.UI.Internal.xaml
             set => SetValue(TextProperty, value);
         }
 
+
+        /// <summary>
+        ///     <see cref="TextColor" />
+        /// </summary>
         public static readonly BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(RadioButton), Color.Black);
 
+        /// <summary>
+        ///     Color of the text in the radiobutton label.
+        ///     This is a bindable property.
+        /// </summary>
         public Color TextColor
         {
             get => (Color)GetValue(TextColorProperty);
