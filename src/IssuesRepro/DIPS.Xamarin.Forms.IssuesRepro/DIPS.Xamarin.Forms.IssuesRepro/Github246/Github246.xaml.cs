9cb3a5e2e7842b424c7c79628e451fdd1756a004
Jonathan Randev <jonathanrandev@users.noreply.github.com>
2020-11-02 15:13:57 +0530

    Fix 246 -  app crash when using box views in trend graph (#247)
    * Fix app crash when using box views in trend graph
    
    * Make a mistake on purpose to trigger the CI build
    
    * Revert "Make a mistake on purpose to trigger the CI build"
    
    This reverts commit 2462047a64062755fd676411953a36bd679f9403.

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github246/Github246.xaml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github246
+{
+    [Issue(246)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Github246 : ContentPage
+    {
+        private readonly Random m_random = new Random();
+
+        public Github246()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            for (var i = 0; i < 50; i++)
+            {
+                var itemWithTrends = new ItemWithTrends {ItemName = $"Item {i}"};
+                itemWithTrends.TrendItems.Add(new TrendItemViewModel(m_random.NextDouble()));
+                itemWithTrends.TrendItems.Add(new TrendItemViewModel(m_random.NextDouble()));
+                itemWithTrends.TrendItems.Add(new TrendItemViewModel(m_random.NextDouble()));
+                ItemsWithTrends.Add(itemWithTrends);
+            }
+        }
+
+        public ObservableCollection<ItemWithTrends> ItemsWithTrends { get; } =
+            new ObservableCollection<ItemWithTrends>();
+    }
+
+    public class ItemWithTrends : INotifyPropertyChanged
+    {
+        public string ItemName { get; set; }
+
+        public ObservableCollection<TrendItemViewModel> TrendItems { get; } =
+            new ObservableCollection<TrendItemViewModel>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class TrendItemViewModel : INotifyPropertyChanged
+    {
+        public TrendItemViewModel(double value)
+        {
+            Value = value;
+        }
+
+        public double Value { get; set; }
+        public int IntValue => (int)Value;
+        public float FloatValue => (float)Value;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
