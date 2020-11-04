7c49137bb58cc7ed9af8f4ee52ce6f2f1f77063d
Iyani Kalupahana <Iyani@CreativeSoftware.com>
2020-11-04 13:16:38 +0530

    Set alignment for buttons in SheetView

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github248/Github248.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github248
+{
+    [Issue(248)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Github248 : ContentPage
+    {
+        public Github248()
+        {
+            InitializeComponent();
+        }
+    }
+}
