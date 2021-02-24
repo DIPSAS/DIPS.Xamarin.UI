a2dde1e10f261e9fa41a9f3ecb84eff2a497e52a
THARIKADAHA80F2\TharikaD <tharika@creativesoftware.com>
2021-02-24 15:57:57 +0530

    Add comments for public properties

--- src/DIPS.Xamarin.UI/Internal/Xaml/ToastContainer.xaml.cs
+++ src/DIPS.Xamarin.UI/Internal/Xaml/ToastContainer.xaml.cs
@@ -3,24 +3,40 @@ using Xamarin.Forms.Xaml;
 
 namespace DIPS.Xamarin.UI.Internal.Xaml
 {
+    /// <summary>
+    ///     Toast container that act as a wrapper to the presented view
+    /// </summary>
     [ContentProperty(nameof(MainContent))]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ToastContainer : ContentView
     {
-        public static readonly BindableProperty MainContentProperty = BindableProperty.Create(
-            nameof(MainContent),
-            typeof(View),
-            typeof(ToastContainer));
-
         public ToastContainer()
         {
             InitializeComponent();
         }
 
+        #region Bindable Properties
+
+        /// <summary>
+        ///     Bindable property for <see cref="MainContent" />
+        /// </summary>
+        public static readonly BindableProperty MainContentProperty = BindableProperty.Create(
+            nameof(MainContent),
+            typeof(View),
+            typeof(ToastContainer));
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets or sets the MainContent for the ToastContainer. This is a bindable property.
+        /// </summary>
         public View MainContent
         {
             get => (View)GetValue(MainContentProperty);
             set => SetValue(MainContentProperty, value);
         }
+        #endregion
     }
 }
