865d39b225d211090f7b6f3e8c49c33ddee076b2
Erling Moxnes Kristiansen <emk@dips.no>
2021-05-06 15:09:24 +0200

    Fixed comments

--- src/DIPS.Xamarin.UI/Controls/Slidable/PanEventArgs.cs
+++ src/DIPS.Xamarin.UI/Controls/Slidable/PanEventArgs.cs
@@ -2,20 +2,23 @@
 
 namespace DIPS.Xamarin.UI.Controls.Slidable
 {
+    /// <summary>
+    ///     Used in <see cref="SlidableLayout.PanEnded" /> and <see cref="SlidableLayout.PanStarted" />
+    /// </summary>
+    /// <param name="currentIndex"></param>
     public class PanEventArgs : EventArgs
     {
         /// <summary>
-        /// Current index when pan event occurs.
-        /// </summary>
-        public int CurrentIndex { get; }
-
-        /// <summary>
-        /// Used in <see cref="SlidableLayout.PanEnded"/> and <see cref="SlidableLayout.PanStarted"/> 
         /// </summary>
         /// <param name="currentIndex"></param>
         public PanEventArgs(int currentIndex)
         {
             CurrentIndex = currentIndex;
         }
+
+        /// <summary>
+        ///     Current index when pan event occurs.
+        /// </summary>
+        public int CurrentIndex { get; }
     }
 }
