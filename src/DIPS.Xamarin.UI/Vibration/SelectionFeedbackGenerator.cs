c6184cc30c2189d6b0fe04b98acc3192db158462
Erling Moxnes Kristiansen <emk@dips.no>
2021-07-12 09:22:36 +0200

    Updated nuspec dependencies and added missing comments.

--- src/DIPS.Xamarin.UI/Vibration/SelectionFeedbackGenerator.cs
+++ src/DIPS.Xamarin.UI/Vibration/SelectionFeedbackGenerator.cs
@@ -7,6 +7,9 @@ namespace DIPS.Xamarin.UI.Vibration
     {
         private readonly IPlatformFeedbackGenerator? m_generator;
 
+        /// <summary>
+        /// Use this for continuous short feedback when a selection changes.
+        /// </summary>
         public SelectionFeedbackGenerator()
         {
             if (Vibration.VibrationService != null)
@@ -15,16 +18,25 @@ namespace DIPS.Xamarin.UI.Vibration
             }
         }
         
+        /// <summary>
+        /// Invoke when a selection has occured. Causes feedback.
+        /// </summary>
         public void SelectionChanged()
         {
             m_generator?.SelectionChanged();
         }
         
+        /// <summary>
+        /// Invoke right before feedback is required. For example at the start of a pan where several selections occur rapidly. 
+        /// </summary>
         public void Prepare()
         {
             m_generator?.Prepare();
         }
 
+        /// <summary>
+        /// Release the underlying iOS generator. Use when this is no longer needed. For example at the end of a pan.
+        /// </summary>
         public void Release()
         {
             m_generator?.Release();
