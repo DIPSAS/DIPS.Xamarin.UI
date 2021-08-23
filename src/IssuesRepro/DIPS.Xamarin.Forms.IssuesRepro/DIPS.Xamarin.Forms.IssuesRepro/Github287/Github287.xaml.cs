6badb28227a1f06086343604f683a0905ccebf00
Eirik Hemstad <ehe@dips.no>
2021-08-23 11:13:04 +0200

    Added ShouldCloseOpenSheets property to SheetBehavior

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github287/Github287.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github287
+{
+    [Issue(287)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Github287 : ContentPage
+    {
+        public Github287()
+        {
+            InitializeComponent();
+        }
+    }
+}
