b05d6d749a9afd0b396b22f9dfb4231c063d209c
Eirik Hemstad <eirikzh@gmail.com>
2021-08-24 09:56:57 +0200

    Feature/ehe 287 (#289)
    * Added ShouldCloseOpenSheets property to SheetBehavior
    
    * Moved property from sheetbehavior to ModalityLayout with handling
    
    * Fixed build issue
    
    * Removed unused code
    
    Co-authored-by: Eirik Hemstad <ehe@dips.no>

---
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github287/Github287.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DIPS.Xamarin.Forms.IssuesRepro.Github287
+{
+    [Issue(287)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Github287 : ContentPage
+    {
+        private bool m_isCloseEnabled;
+
+        public Github287()
+        {
+            InitializeComponent();
+            IsCloseEnabled = true;
+        }
+
+        public bool IsCloseEnabled
+        {
+            get => m_isCloseEnabled;
+            set
+            {
+                m_isCloseEnabled = value;
+                OnPropertyChanged(nameof(IsCloseEnabled));
+            }
+        }
+    }
+}
