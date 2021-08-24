5004a95231280b2373414701784575e9c04c0989
Eirik Hemstad <ehe@dips.no>
2021-08-24 09:50:57 +0200

    Removed unused code

--- src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github287/Github287.xaml.cs
+++ src/IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/DIPS.Xamarin.Forms.IssuesRepro/Github287/Github287.xaml.cs
@@ -14,34 +14,12 @@ namespace DIPS.Xamarin.Forms.IssuesRepro.Github287
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Github287 : ContentPage
     {
-        private bool m_isSheetAOpen;
-        private bool m_isSheetBOpen;
         private bool m_isCloseEnabled;
 
         public Github287()
         {
             InitializeComponent();
             IsCloseEnabled = true;
-        }
-
-        public bool IsSheetAOpen
-        {
-            get => m_isSheetAOpen;
-            set
-            {
-                m_isSheetAOpen = value;
-                OnPropertyChanged(nameof(IsSheetAOpen));
-            }
-        }
-
-        public bool IsSheetBOpen
-        {
-            get => m_isSheetBOpen;
-            set
-            {
-                m_isSheetBOpen = value;
-                OnPropertyChanged(nameof(IsSheetBOpen));
-            }
         }
 
         public bool IsCloseEnabled
@@ -52,16 +30,6 @@ namespace DIPS.Xamarin.Forms.IssuesRepro.Github287
                 m_isCloseEnabled = value;
                 OnPropertyChanged(nameof(IsCloseEnabled));
             }
-        }
-
-        private void MenuItemA_OnClicked(object sender, EventArgs e)
-        {
-            IsSheetAOpen = true;
-        }
-        
-        private void MenuItemB_OnClicked(object sender, EventArgs e)
-        {
-            IsSheetAOpen = !IsSheetAOpen;
         }
     }
 }
