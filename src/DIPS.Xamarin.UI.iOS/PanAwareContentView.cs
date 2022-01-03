9efbbe5e936b4978843edec13360898703c5c56c
Erling Moxnes Kristiansen <emk@dips.no>
2022-01-03 11:04:58 +0100

    Added PanAwareContentView to iOS

---
+++ src/DIPS.Xamarin.UI.iOS/PanAwareContentView.cs
@@ -0,0 +1,26 @@
+﻿
+using DIPS.Xamarin.UI.Controls.Slidable;
+using DIPS.Xamarin.UI.Internal.Xaml.Sheet;
+using DIPS.Xamarin.UI.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SheetView), typeof(PanAwareContentView))]
+
+namespace DIPS.Xamarin.UI.iOS
+{
+    public class PanAwareContentView : VisualElementRenderer<ContentView>
+    {
+        private UIView m_uiView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ContentView> e)
+        {
+            base.OnElementChanged(e);
+
+            m_uiView = GetControl();
+
+            var uiViewSubviews = m_uiView.Subviews;
+        }
+    }
+}
